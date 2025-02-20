44acb25a0f97d38e696f36dc3e5289cfe7565726
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2025-02-20 09:08:12 +0100

    Implementacion en servicios de buscarPorCategoria y buscarPorNombreCategoria

--- Pedidos/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/servicio/impl/ProductoServicioImpl.java
+++ Pedidos/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/servicio/impl/ProductoServicioImpl.java
@@ -1,18 +1,24 @@
 package com.hlc.cliente_uno_a_muchos_pedido.servicio.impl;
 
+import com.hlc.cliente_uno_a_muchos_pedido.entidad.Categoria;
 import com.hlc.cliente_uno_a_muchos_pedido.entidad.Producto;
+import com.hlc.cliente_uno_a_muchos_pedido.repositorio.CategoriaRepository;
 import com.hlc.cliente_uno_a_muchos_pedido.repositorio.ProductoRepository;
 import com.hlc.cliente_uno_a_muchos_pedido.servicio.ProductoServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductoServicioImpl implements ProductoServicio {
 
     @Autowired
     private ProductoRepository productoRepository;
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     @Override
     public Producto guardarProducto(Producto producto) {
@@ -44,4 +50,16 @@ public class ProductoServicioImpl implements ProductoServicio {
     public void eliminarProducto(Long id) {
         productoRepository.deleteById(id);
     }
+
+	@Override
+	public List<Producto> buscarPorCategoria(Categoria categoria) {
+		return productoRepository.findByCategoria(categoria); 
+	}
+
+	@Override
+	public List<Producto> buscarPorNombreCategoria(String nombre_categoria) {
+		Optional<Categoria> categoria =   categoriaRepository.findByNombreIgnoreCase(nombre_categoria);   
+		List<Producto> productos = productoRepository.findByCategoria(categoria.get());   
+		return productos; 
+	}
 }
