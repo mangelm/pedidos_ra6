dbd0f3ec0ca762f9578ef23aaf9ef0c39545f26c
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2025-02-20 19:59:27 +0100

    Arreglo Home Controlador Test Ok

--- Pedidos/src/test/java/com/hlc/cliente_uno_a_muchos_pedido/controlador/HomeControladorTest.java
+++ Pedidos/src/test/java/com/hlc/cliente_uno_a_muchos_pedido/controlador/HomeControladorTest.java
@@ -1,25 +1,42 @@
 package com.hlc.cliente_uno_a_muchos_pedido.controlador;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.hlc.cliente_uno_a_muchos_pedido.servicio.ProductoServicio;
+
 class HomeControladorTest {
 
     private MockMvc mockMvc;
-
+    
+    @Mock
+    private ProductoServicio productoServicio;
+    
+    @InjectMocks
+    private HomeControlador homeControlador; 
+    
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.openMocks(this);  // Inicializa los mocks
 
-        // Configurar MockMvc para testear controladores sin servidor
-        mockMvc = MockMvcBuilders.standaloneSetup(new HomeControlador()).build();
+        // Evita NullPointerException devolviendo una lista vacía
+        when(productoServicio.obtenerTodasLasCategorias()).thenReturn(Collections.emptyList());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(homeControlador).build();
     }
 
     @Test
